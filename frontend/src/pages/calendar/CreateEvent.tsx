90220e95f98a2f0851d471a01972f5272105d34b
Bernardo Meireles <bernardo.meireles@tecnico.ulisboa.pt>
2024-03-17 06:13:10 +0000

    Event creation and retrieval

--- frontend/src/pages/calendar/CreateEvent.tsx
+++ frontend/src/pages/calendar/CreateEvent.tsx
@@ -16,30 +16,51 @@ const CreateEvent: React.FC = () => {
   const [title, setTitle] = useState('');
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
-  const [events, setEvents] = useState<Event[]>([]);
 
-  const handleCreateEvent = () => {
+  const handleCreateEvent = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     if (title && startDate && endDate) {
-      setEvents([...events, { title, start: startDate, end: endDate }]);
-      setTitle('');
-      setStartDate(null);
-      setEndDate(null);
+      console.log('Creating event');
+      const token = localStorage.getItem('token');
+      fetch('http://localhost:5000/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          title: title,
+          start: moment(startDate).format(),
+          end: moment(endDate).format()
+        })
+      }).then((response) => {
+        if (response.ok) {
+          console.log("Form submitted successfully");
+        } else {
+          console.log("Error:", response.status);
+        }
+      });
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'title') {
+      setTitle(value);
     }
   };
 
   return (
-    <div className="h-screen bg-white flex flex-col items-center justify-center px-6 relative">
+    <div className="h-screen bg-white flex flex-col items-center justify-center px-24 relative">
       <img src={waves} alt="Waves" className="absolute bottom-0 left-0 w-full h-full" />
       <div className="z-10 relative w-full space-y-4">
         <Navbar selected={2} />
         <h1 className="text-3xl font-bold text-center mb-8">Create New Event</h1>
-        <div className="bg-white border rounded-lg p-2 space-y-4">
-          <input
-            type="text"
-            placeholder="Event title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+        <form onSubmit={handleCreateEvent} className="space-y-4">
+          <div className="border p-4 rounded bg-white">
+            <label htmlFor="title" className="block font-bold mb-2 text-center border-b-2">Event Title</label>
+            <input type="text" name="title" id="title" value={title} onChange={handleInputChange} className="w-full text-center border" />
+          </div>
           <div className="datepicker-container space-y-4">
             <DatePicker
               selected={startDate}
@@ -49,6 +70,7 @@ const CreateEvent: React.FC = () => {
               timeIntervals={15}
               dateFormat="MMMM d, yyyy h:mm aa"
               placeholderText="Start date and time"
+              className="rounded-md text-center"
             />
             <DatePicker
               selected={endDate}
@@ -58,12 +80,13 @@ const CreateEvent: React.FC = () => {
               timeIntervals={15}
               dateFormat="MMMM d, yyyy h:mm aa"
               placeholderText="End date and time"
+              className="rounded-md text-center"
             />
           </div>
-        </div>
-        <div className="flex justify-center mt-8">
-          <button onClick={handleCreateEvent} className="py-2 px-11 bg-blue-500 text-white rounded-lg font-bold">Create Event</button>
-        </div>
+          <div className="flex justify-center mt-4">
+            <button type="submit" className="py-2 px-11 bg-blue-500 text-white rounded-lg font-bold">Create Event</button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- frontend/src/pages/calendar/MyCalendar.tsx
+++ frontend/src/pages/calendar/MyCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -8,21 +8,63 @@ import CreateEvent from './CreateEvent';
 
 const localizer = momentLocalizer(moment)
 
-const events = [
-  {
-    title: 'Event 1',
-    start: moment().toDate(),
-    end: moment().add(4, 'hours').toDate()
-  },
-  // more events...
-];
 interface Event {
+  _id: string;
+  email: string;
   title: string;
   start: Date;
   end: Date;
 }
 
 function MyCalendar() {
+
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:5000/events', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("chegou aqui");
+
+      //   {
+      //     "data": [
+      //         {
+      //             "_id": "65f688737d328ab5ec350550",
+      //             "email": "bcmeireles74@gmail.com",
+      //             "end": "2024-03-17T16:15:00+00:00",
+      //             "start": "2024-03-17T06:00:00+00:00",
+      //             "title": "asdasdasd"
+      //         }
+      //     ],
+      //     "status": "success",
+      //     "success": true,
+      //     "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMDY1NTg3NCwianRpIjoiMzgzNTNmN2UtN2NiNC00NWFhLWE4YjQtYWQ1NzYwNGZlY2QwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImJjbWVpcmVsZXM3NEBnbWFpbC5jb20iLCJuYmYiOjE3MTA2NTU4NzQsImNzcmYiOiIzNmMzMzE5ZC01YjMxLTQ3NGQtYjZmNy0yNjY2ZDY5NmM4YmQiLCJleHAiOjE3MTA2NTY3NzR9.VVC9RxLAka00FT_xaBBWs7KZu_1QkcuI97Vm9HlxIWA"
+      // }
+
+        const events = data.data.map((event: Event) => {
+          return {
+            ...event,
+            start: new Date(event.start),
+            end: new Date(event.end)
+          }
+        });
+        setEvents(events);
+      }
+    };
+
+    fetchData();
+  
+  })
+
   return (
     <div className="h-screen bg-white flex items-center justify-center px-6 relative">
       <img src={waves} alt="Waves" className="absolute bottom-0 left-0 w-full h-full" />
